32366c417846685c9fa745fe0a44ca8eb580961b
yefuxiong <yefuxiong@yeah.net>
2020-03-06 14:13:44 +0800

    '000'

---
+++ assets/js/chunk-2d0d6cf3.0c3d7a64.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d6cf3"],{"73ab":function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("main",[n._v(" 现在还未有页面 ")])},c=[],u={name:"industry"},r=u,s=t("2877"),i=Object(s["a"])(r,a,c,!1,null,"9d6e18e4",null);e["default"]=i.exports}}]);
